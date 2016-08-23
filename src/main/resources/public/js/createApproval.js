c3499ae76e8e9cd7700c15ec3ca3d2554865c215
Ayush Jindal <ayush.j@directi.com>
2016-08-23 21:43:32 +0530

    Get requestor and approver from query params

--- src/main/resources/public/js/createApproval.js
+++ src/main/resources/public/js/createApproval.js
@@ -2,9 +2,17 @@ $(document).ready(function()
 {
      var amount;
      $("#submit").click(function () {
+        console.log("submit clicked")
+         var queryString = window.location.search.substring(1);
          amount = $("#amount").val();
-         requestorId = "u:007effelfl6l6ajf";
-         approverId = "u:3y373y1tty31368w";
+         console.log("amount: " + amount);
+         event = decodeURIComponent(getQueryVariable("flockEvent"));
+         console.log("event: " + event);
+         var parsedEvent = JSON.parse(event);
+         console.log("userId: " + parsedEvent.userId);
+         console.log("chat: " + parsedEvent.chat);
+         requestorId = parsedEvent.userId;
+         approverId = parsedEvent.chat;
          generateApprovalRequest(amount);
          flock.close();
      });
@@ -19,6 +27,18 @@ $(document).ready(function()
           sendAjaxRequest(baseUrl + "create", "post", payload, function (response) {
               console.log("successful response: " + JSON.stringify(response));
           });
+     }
+
+     function getQueryVariable(variable) {
+         var query = window.location.search.substring(1);
+         var vars = query.split("&");
+         for (var i = 0; i < vars.length; i++) {
+             var pair = vars[i].split("=");
+             if (pair[0] == variable) {
+                 return pair[1];
+             }
+         }
+         return null;
      }
 });
 
