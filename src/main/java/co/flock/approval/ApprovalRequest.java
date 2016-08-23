a11b7b0781b61912c4048f54e78e6167b03e04b7
Ayush Jindal <ayush.j@directi.com>
2016-08-23 15:58:04 +0530

    Accept requestorId and approverId in create API

--- src/main/java/co/flock/approval/ApprovalRequest.java
+++ src/main/java/co/flock/approval/ApprovalRequest.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class ApprovalRequest
 {
     private int amount;
+    private String requestorId;
+    private String approverId;
 
     @JsonProperty("amount")
     public int getAmount()
@@ -12,11 +14,25 @@ public class ApprovalRequest
         return amount;
     }
 
+    @JsonProperty("requestorId")
+    public String getRequestorId()
+    {
+        return requestorId;
+    }
+
+    @JsonProperty("approverId")
+    public String getApproverId()
+    {
+        return approverId;
+    }
+
     @Override
     public String toString()
     {
         return "ApprovalRequest{" +
                 "amount=" + amount +
+                ", requestorId='" + requestorId + '\'' +
+                ", approverId='" + approverId + '\'' +
                 '}';
     }
 }
--- src/main/resources/public/js/createApproval.js
+++ src/main/resources/public/js/createApproval.js
@@ -3,11 +3,18 @@ $(document).ready(function()
      var amount;
      $("#submit").click(function () {
          amount = $("#amount").val();
+         requestorId = "u:123";
+         approverId = "u:456";
          generateApprovalRequest(amount);
      });
 
      function generateApprovalRequest(amount) {
-          payload = {"amount": + amount};
+          payload =
+          {
+              "amount": amount,
+              "requestorId": requestorId,
+              "approverId": approverId
+          };
           sendAjaxRequest(baseUrl + "create", "post", payload, function (response) {
               console.log("successful response: " + JSON.stringify(response));
           });
