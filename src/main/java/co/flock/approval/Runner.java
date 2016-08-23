808c1e94fae6acf8b434f1962c141547e9d2c1b1
anshuman <anshumanbajpai123@gmail.com>
2016-08-23 13:11:05 +0530

    setup basic spark server

--- src/main/java/co/flock/approval/Runner.java
+++ src/main/java/co/flock/approval/Runner.java
@@ -1,8 +1,23 @@
 package co.flock.approval;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
 public class Runner {
 
     public static void main(String[] args) {
 
+        port(9000);
+
+        get("/", (req, res) -> {
+            System.out.println("Req : " + req.body());
+            return "hello get";
+        });
+
+        post("/", (req, res) -> {
+            System.out.println("Req : " + req.body());
+            return "hello post";
+        });
     }
 }
